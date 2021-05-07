e2cf80c7c99145ff3e4f85b750c5840f2d159fc5
Eithar-Yahia <eitharyahia444@gmail.com>
2021-05-07 13:17:27 +0200

    complete group

--- SocialNetwork/Groups/models.py
+++ SocialNetwork/Groups/models.py
@@ -6,5 +6,7 @@ from Users.models import CustomUser
 class Group(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
     creation_date = models.DateField(auto_now=True)
     members = models.ManyToManyField(CustomUser, related_name="joinedgroups")
+    requests = models.ManyToManyField(CustomUser, related_name="requestedgroups")
